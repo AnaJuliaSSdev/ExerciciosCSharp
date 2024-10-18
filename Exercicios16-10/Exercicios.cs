3a875c8c39851a862874f06b2ab0d85ee26200fd
AnaJuliaSSdev <anajuliassdev@gmail.com>
2024-10-18 12:15:03 -0300

    add: exercicio elevador

--- Exercicios16-10/Exercicios.cs
+++ Exercicios16-10/Exercicios.cs
@@ -9,22 +9,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j <= i; j++)
+            for (int i = 0; i < 6; i++)
             {
-                Console.Write("*");
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
             }
-            Console.WriteLine("\n");
-        }
-        for (int i = 0; i <= 4; i++)
-        {
-            for (int j = 4; j > i; j--)
+            for (int i = 0; i <= 4; i++)
             {
-                Console.Write("*");
+                for (int j = 4; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
             }
-            Console.WriteLine("\n");
-        }
         Console.ReadKey();
     }
 }
